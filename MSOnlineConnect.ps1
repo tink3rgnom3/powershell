5cfec9bbf6336d5d2800ee1f6362426b15ff29e8
tink3rgnom3 <ajmartinez1982@gmail.com>
2018-09-04 13:18:13 -0400

    Update MSOnlineConnect.ps1
    Added loop to display menu at start.

--- MSOnlineConnect.ps1
+++ MSOnlineConnect.ps1
@@ -1,12 +1,3 @@
-Write-host "
-Available Commands:
-O365Login - Log into Office 365 online
-AzureADLogin - Log into Azure Active Directory
-
-Type a command to start.
-
-To close session, type 'exit'
-"
 
 #Office 365 Login
 Function O365Login(){
@@ -21,14 +12,14 @@ Function O365Login(){
 function AzureADLogin (){
     If($UserCredential -eq $Null){
         $AzureUserCred = Get-Credential
-    } 
+    }
 
     Else{
         $AzureUserCred = $UserCredential
     }
 
     $ADSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-    #Install-Module -Name AzureAD 
+    #Install-Module -Name AzureAD #Use this if needed
     Connect-MsolService -Credential $AzureUserCred
     Connect-AzureAD -Credential $AzureUserCred
     #(Get-Item C:\Windows\System32\WindowsPowerShell\v1.0\Modules\MSOnline\Microsoft.Online.Administration.Automation.PSModule.dll).VersionInfo.FileVersion
@@ -39,3 +30,28 @@ function EndSession(){
     Remove-PSSession $Session
     Write-Host 'Session has ended. Type "exit" to close.'
 }
+
+do{
+    Write-host "Available Commands:
+
+1. O365Login - Log into Office 365 online
+2. AzureADLogin - Log into Azure Active Directory
+
+To start, type the command number and Enter
+
+To close session, type 'exit'
+    "
+    $Answer = Read-Host
+    If($Answer -eq '1'){
+        O365Login
+        break
+    }
+    If($Answer -eq '2'){
+        AzureADLogin
+        break
+    }
+    Else{Write-Host -ForegroundColor Red "
+Sorry, that menu item is not valid
+    "}
+}
+until($Answer -eq ('1' -or '2'))
