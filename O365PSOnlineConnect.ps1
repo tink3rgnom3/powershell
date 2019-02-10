2055f150004d0c839dd1b0106e18cfcdfcf71157
tink3rgnom3 <ajmartinez1982@gmail.com>
2019-02-09 20:20:34 -0500

    Update and rename MSOnlineConnect.ps1 to O365PSOnlineConnect.ps1
    Added function to check for Powershell v5 or higher and whether MSOnline module is installed.
    If not installed, it will try to do so.

--- O365PSOnlineConnect.ps1
+++ O365PSOnlineConnect.ps1
@@ -1,11 +1,24 @@
+Set-ExecutionPolicy RemoteSigned -Force
+
+$PSVersionCheck = $PSVersionTable.PSVersion.Major
+If($PSVersionCheck -ge 5){
+    $MSOnlineCheck = Get-InstalledModule -Name MSOnline -ErrorAction SilentlyContinue
+    }
+
+
+If($MSOnlineCheck -eq $Null){
+    
+    Install-Module MSOnline -Confirm:$False
+    
+    }
 
 #Office 365 Login
 Function O365Login(){
-    Set-ExecutionPolicy RemoteSigned
+
     $Global:UserCredential = Get-Credential
     $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
     Connect-MsolService -Credential $UserCredential
-    Import-PSSession $Session
+    Import-PSSession $Session -AllowClobber
 }
 
 #Azure AD login
