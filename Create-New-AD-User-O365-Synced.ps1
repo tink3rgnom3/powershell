315552ab31b9e1bafe23c19ef9ff005ea0ddd747
tink3rgnom3 <ajmartinez1982@gmail.com>
2019-02-09 20:15:53 -0500

    Add files via upload
    Added script to create new AD users in Office 365 AD-synced environments
    Added CSV sheets for user list and parameters

---
+++ Create-New-AD-User-O365-Synced.ps1
@@ -0,0 +1,76 @@
+﻿cd C:\Source\Scripts
+Import-Module ActiveDirectory
+
+$Userlist = Import-Csv .\Create-New-AD-User-O365-Synced-List.csv
+$Parameters = Import-Csv .\Create-New-AD-User-O365-Synced-Params.csv
+$EmailDomain = $Parameters.EmailDomain
+$Path = $Parameters.Path
+$EmailUsername = $Parameters.EmailFormat
+$Clientmsdomain = $Parameters.MSDomain
+
+ForEach($NewUser in $Userlist){
+    $FirstName = $NewUser.FirstName
+    $LastName = $NewUser.Lastname
+    $FullName = "$Firstname $LastName"
+    $UserName = $NewUser.Username
+    $FirstInitial = $FirstName[0]
+    $LastInitial = $LastName[0]
+    $Principal = "$Username@$env:USERDNSDOMAIN"
+    $Password = (ConvertTo-SecureString -String ($NewUser.Passwd) -AsPlainText -Force)
+    $Description = $NewUser.Description
+    $Department = $NewUser.Department
+    $UserToCopy = $NewUser.UserToCopy
+    If($NewUser.EmailDomain -ne $Null){
+        $EmailDomain = $NewUser.EmailDomain
+    }
+    $EmailAddr = "$($EmailUsername)@$EmailDomain"
+
+    
+    #Check if user exists
+    $UserExists = Get-ADUser -Filter {Name -eq $FullName} -ErrorAction SilentlyContinue
+    If ($UserExists -eq $Null){
+        New-ADUser -Name $FullName -GivenName $FirstName -Surname $LastName -DisplayName $FullName -SamAccountName $UserName -AccountPassword $Password  -UserPrincipalName $Principal -Description $Description -Enabled:$True -Department $Department
+        Set-ADUser $Username -UserPrincipalName "$Username@$EmailDomain"
+        Write-Host "User $Username has been created"
+        Get-ADUser $Username
+    }
+    Else{
+        Write-Host "User $Fullname already exists"
+    }
+
+    $PrimarySMTP = "SMTP:"
+    $SecondarySMTP = "smtp:"
+
+    $UserFullName = $FirstName + $LastName
+
+    $UserMSEmail = "$Username@$ClientMSDomain"
+
+    $ProxyPrimary = $PrimarySMTP + $EmailAddr
+    $ProxySecondary = $SecondarySMTP + $UserMSEmail
+
+
+    Get-ADUser -Identity $Username | Set-ADUser -Add @{ProxyAddresses="$ProxyPrimary"}
+    Get-ADUser -Identity $Username | Set-ADUser -Add @{ProxyAddresses="$ProxySecondary"}
+    Get-AdUser -Identity $Username | Set-ADUser -Replace @{mail="$EmailAddr"}
+
+        #Copy groups from another user
+    If($UserToCopy -ne $Null){
+        $SourceUser = Get-ADUser $UserToCopy -ErrorAction SilentlyContinue
+        }
+
+    If($SourceUser -ne $Null){
+        $UserGroups = Get-ADPrincipalGroupMembership $SourceUser | Where {$_.Name -ne "Domain Users"}
+        ForEach($Group in $UserGroups){
+            Add-ADGroupMember -Identity $Group -Members $Username -ErrorAction SilentlyContinue
+            Write-Host "Copying user $Username to $Groupname"
+            }
+
+        }
+
+    Else{
+        Write-Host "No groups to copy"
+    }
+    
+}
+
+.\AD_Sync.ps1
