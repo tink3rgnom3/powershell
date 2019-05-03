0dd5cee7a89b0c2daf1a8e96cfd5ef55964f6313
tink3rgnom3 <ajmartinez1982@gmail.com>
2019-05-03 18:38:02 -0400

    Script for logging into O365 MSOnline & Exchange

---
+++ O365PSOnlineConnect.ps1
@@ -0,0 +1,22 @@
+﻿Set-ExecutionPolicy RemoteSigned -Force
+
+$PSVersionCheck = $PSVersionTable.PSVersion.Major
+If($PSVersionCheck -ge 5){
+    $MSOnlineCheck = Get-InstalledModule -Name MSOnline -ErrorAction SilentlyContinue
+    
+	If($MSOnlineCheck -eq $Null){
+		Install-Module MSOnline -Confirm:$False
+    }    
+}
+    
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Connect-MsolService -Credential $UserCredential
+Import-PSSession $Session -AllowClobber
+
+
+#End the session
+function EndSession(){
+    Remove-PSSession $Session
+    Write-Host 'Session has ended. Type "exit" to close.'
+}
