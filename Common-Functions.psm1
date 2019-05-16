1d2e8d47f0eade6d539d3a38e8494ab9a21b74f8
tink3rgnom3 <ajmartinez1982@gmail.com>
2019-05-16 10:55:21 -0400

    Created common functions module
    Consolidated commonly used functions in one module to avoid repetition in scripts

---
+++ Common-Functions.psm1
@@ -0,0 +1,20 @@
+﻿
+function MSOLConnected {
+    Get-MsolDomain -ErrorAction SilentlyContinue | out-null
+    $result = $?
+    return $result
+}
+
+function MSOnlineConnect(){    
+	$UserCredential = Get-Credential
+    Write-Host "Enter Office 365 admin credentials when prompted"
+	$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+	Connect-MsolService -Credential $UserCredential
+	Import-PSSession $Session -AllowClobber
+}
+
+function SyncADtoO365(){
+    Set-ExecutionPolicy Unrestricted -Force
+    Import-Module ADSync
+    Start-ADSyncSyncCycle -PolicyType Delta
+}
