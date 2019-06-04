8be2920eb7bcc00ac2d7c91a74d0395c2ed789a3
tink3rgnom3 <ajmartinez1982@gmail.com>
2019-06-04 17:31:36 -0400

    Offloaded functions to module, added forwarding
    Removed MSOLConnected and LogWrite functions, which are now in Common-Functions.psm1
    Added $mailbox parameter if connected to O365
    Added mail forwarding and setting mailbox to shared if $Mailbox is not null

--- Bulk-User-Offboarding-O365-Synced.ps1
+++ Bulk-User-Offboarding-O365-Synced.ps1
@@ -1,29 +1,22 @@
-cd C:\source\scripts\
-Import-Module ActiveDirectory
+cd C:\Source\Scripts
 Import-Module .\Common-Functions.psm1
 
-function MSOLConnected {
-    Get-MsolDomain -ErrorAction SilentlyContinue | out-null
-    $result = $?
-    return $result
+If (-Not (CheckRunningAsAdmin)){
+    Write-Host "You are not currently running as admin. Please relaunch as admin."
+    exit
 }
+
+Import-Module ActiveDirectory
+
 
 #Connect to MS Online
 If (-Not (MSOLConnected)){
-    .\O365PSOnlineConnect.ps1
+    MSOnlineConnect
     Write-Host "Enter Office 365 admin credentials when prompted"
 }
 
 $Logfile = "C:\Source\Scripts\Bulk-User-Offboarding-O365-Synced-Log.log"
-
-Function LogWrite
-{
-   Param ([string]$logstring)
-
-   $TimeStamp = get-date -uformat "%Y/%m/%d %H:%M"
-
-   Add-content $Logfile -value "[$TimeStamp] $logstring"
-}
+#See Common-Functions.psm1 for LogWrite function
 
 $ScriptParams = Import-Csv .\Bulk-User-Offboarding-O365-Synced-Params.csv
 $Userlist = Import-Csv .\Bulk-User-Offboarding-O365-Synced-List.csv
@@ -41,7 +34,11 @@ ForEach($OffUser in $Userlist){
     $FullName = "$Firstname $Lastname"
     $Username = Get-ADUser -Filter {Name -eq $FullName}
     $FLastName = $FirstName[0] + $LastName
+    $ForwardingAddress = $Offuser.ForwardingAddress
 
+    If(MSOLConnected){
+        $Mailbox = Get-Mailbox -Identity $FullName
+    }
     #Disable AD User
     Set-ADUser $Username -Enabled $False
     LogWrite "User $FullName is disabled"
@@ -57,19 +54,39 @@ ForEach($OffUser in $Userlist){
         LogWrite "$Groupname"
 
     }
-
-    #Remove proxy attributes
+    #Clear mail field in AD
     Set-ADUser -Identity $Username -Clear Mail
-    Set-ADUser -Identity $Username -Clear ProxyAddresses
-    Set-ADUser -Identity $Username -Add @{ProxyAddresses = "SMTP:$FLastName@$MSDomain"}
-    LogWrite "Cleared proxy addresses"
+    #Set to hide from address lists
     Set-ADUser -Identity $Username -Replace @{msExchHideFromAddressLists=$TRUE}
+    #Attribute must be set to sync in AD connect and a transform rule must be in place
+    Set-ADUser -Identity $Username -Replace @{"msDS-CloudExtensionAttribute1"="HideFromGAL"}
     LogWrite "Removed $Fullname from global address list"
 
     #Move user to "Disabled Users" OU
     Get-ADUser $Username | Move-ADObject -TargetPath $DisabledUserPath
     LogWrite "User $FullName has been moved to Disabled Users"
 
+    If ($Mailbox){
+        #Set mailbox to shared
+        If ($Mailbox.isShared -eq $False){
+            Set-mailbox -Identity $Mailbox.alias -Type Shared
+            Write-Host "Setting mailbox for $FullName to Shared"
+        }
+        Else{
+            Write-Host "Mailbox is already shared"
+        }
+        #Forward mailbox
+        If ($ForwardingAddress -ne $Null){
+            Set-mailbox -ForwardingAddress $ForwardingAddress
+            Write-Host "Forwarding mailbox to $ForwardingAddress"
+        }
+        Else{
+            Write-Host "Could not forward mailbox"
+        }
+    }
+    Else{
+        Write-Host "Please log into Office 365 to set mailbox to shared and forward"
+    }
 
     Write-Host "User offboarding for $FullName is complete"
 }
