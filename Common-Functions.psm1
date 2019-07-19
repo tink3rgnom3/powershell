d1bc4987fe2d9bd82d8879a16ca2f5d0213724cd
tink3rgnom3 <ajmartinez1982@gmail.com>
2019-07-19 17:56:17 -0400

    Consolidated functions from scripts
    Added functions CheckRunningAsAdmin, ExchConnected, setO365License

--- Common-Functions.psm1
+++ Common-Functions.psm1
@@ -1,5 +1,23 @@
+function CheckRunningAsAdmin(){
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
 function ConnectPremExch($MailServer,$LocalDomain){
     New-PSSession -ConfigurationName Microsoft.exchange -ConnectionUri "http://$MailServer.$LocalDomain/powershell"
+}
+
+function ExchConnected {
+    Get-ExchangeServer -ErrorAction SilentlyContinue | out-null
+    $result = $?
+    return $result
+}
+
+Function LogWrite
+{
+   Param ([string]$logstring)
+   $TimeStamp = get-date -uformat "%Y/%m/%d %H:%M"
+   Add-content $Logfile -value "[$TimeStamp] $logstring"
 }
 
 function MSOLConnected {
@@ -14,6 +32,54 @@ function MSOnlineConnect(){
 	$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 	Connect-MsolService -Credential $UserCredential
 	Import-PSSession $Session -AllowClobber
+}
+
+function setO365License($Domain,$MSClient,$MSDomain){
+
+    If (-Not (MSOLConnected)){
+        MSOnlineConnect
+    }
+    $USR = get-msoluser | Where-Object{($_.Firstname -eq $Firstname) -and ($_.Lastname -eq $Lastname)}
+    #This script will fetch license numbers and assign a license based on a chosen number.
+    
+    $Menu = @()
+    $Licenses = Get-MsolAccountSku
+    $Place = 0
+    $ItemNumber = 1
+
+    If($USR.UserPrincipalName -match "onmicrosoft.com"){
+        Set-MsolUserPrincipalName -ObjectId $UserObjId -NewUserPrincipalName "$UserAlias@$Domain"
+    }
+
+    If(-Not ($USR.UsageLocation)){
+        Set-MsolUser -ObjectId $UserObjId -UsageLocation "US"
+    }
+
+    ForEach($Account in $Licenses){
+        
+        $LicenseNumber = $Account.ActiveUnits - $Account.ConsumedUnits
+        If($LicenseNumber -gt 0){
+            $Menu += $Account.SkuPartNumber
+            Write-Host "$ItemNumber : $Menu[$Place] ($LicenseNumber available)"
+            $Place++
+            $ItemNumber++
+        }
+        
+    }
+
+    Do{
+
+        [int]$Question = Read-Host -Prompt "Enter an item number"
+        [int]$Answer = $Question - 1
+    
+        If(($Answer -gt $Menu.Length) -and ($answer -le 0)){
+            Write-host "Sorry, that option isn't available"
+        }
+    } until(($Answer -lt $Menu.Length) -and ($answer -ge 0))
+
+    $UserLicense = $MSClient+ ":" + $Menu[$Answer]
+    Set-MsolUserLicense -ObjectId $UserObjId -AddLicenses $UserLicense
+    Write-Host " Assigned $UserLicense to $User"
 }
 
 function SyncADtoO365(){
