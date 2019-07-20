83228feaf91a4193878fb6cf492e0df4959f14d3
tink3rgnom3 <ajmartinez1982@gmail.com>
2019-07-20 00:08:31 -0400

    Changed $Parameters to $ScriptParams
    Changed $Parameters to $ScriptParams for consistency across scripts

--- Bulk-User-Offboarding-O365-Synced.ps1
+++ Bulk-User-Offboarding-O365-Synced.ps1
@@ -3,97 +3,111 @@ Import-Module .\Common-Functions.psm1
 
 If (-Not (CheckRunningAsAdmin)){
     Write-Host "You are not currently running as admin. Please relaunch as admin."
+    Start-Sleep -Seconds 10
     exit
 }
-
 Import-Module ActiveDirectory
-
 
 #Connect to MS Online
 If (-Not (MSOLConnected)){
-    .\O365PSOnlineConnect
+    MSOnlineConnect
     Write-Host "Enter Office 365 admin credentials when prompted"
 }
 
-$Logfile = "C:\Source\Scripts\Bulk-User-Offboarding-O365-Synced-Log.log"
-#See Common-Functions.psm1 for LogWrite function
-
+$Userlist = Import-Csv .\Create-New-AD-User-O365-Synced-List.csv
 $ScriptParams = Import-Csv .\ADDS-O365-Synced-Params.csv
-$Userlist = Import-Csv .\Bulk-User-Offboarding-O365-Synced-List.csv
-$DisabledUserPath = $ScriptParams.DisabledUserPath
-$MSDomain = $ScriptParams.MSDomain
+$EmailDomain = $ScriptParams.EmailDomain
+$UserPath = $ScriptParams.UserPath
+$EmailConvention = $ScriptParams.EmailFormat
+$Clientmsdomain = $ScriptParams.MSDomain
 
-If(-Not ($DisabledUserPath)){
-    $DisabledUserPath = Get-ADOrganizationalUnit -Filter * | Where-Object {($_.DistinguishedName -like "OU=Users,OU=Disabled*") -or ($_.Name -eq "Disabled Accounts" -or "Disabled Users")} | ForEach-Object{$_.DistinguishedName}
-}
-
-ForEach($OffUser in $Userlist){
-
-    $FName = $Offuser.FirstName
-    $LName = $Offuser.LastName
-    $FullName = "$FName $LName"
-    $Username = Get-AdUser -Filter {(GivenName -eq $FName) -and (Surname -eq $LName)} | ForEach-Object{$_.SamAccountName}
-    $FLName = $FName[0] + $LName
-    $ForwardingAddress = $Offuser.ForwardingAddress
-
-    If(MSOLConnected){
-        $Mailbox = Get-Mailbox -Identity $FullName
-        $MsolUser = Get-MsolUser | Where-Object{($_.FName -eq $FName) -and ($_.LName -eq $LName)}
+ForEach($NewUser in $Userlist){
+    $FirstName = $NewUser.FirstName
+    $LastName = $NewUser.Lastname
+    $FullName = "$Firstname $LastName"
+    $UserName = $NewUser.Username
+    $FirstInitial = $FirstName[0]
+    $LastInitial = $LastName[0]
+    $Principal = "$Username@$env:USERDNSDOMAIN"
+    $Password = (ConvertTo-SecureString -String ($NewUser.Passwd) -AsPlainText -Force)
+    $Description = $NewUser.Description
+    $Department = $NewUser.Department
+    $UserToCopy = $NewUser.UserToCopy
+#   If(($NewUser.UserPath -ne $Null) -or ($NewUser.UserPath -ne "")){
+#       $UserPath = $NewUser.UserPath
+#    Else{
+#       $UserPath = $ScriptParams.UserPath
+#    }
+    If($EmailConvention -eq "FirstNameLastName"){
+        $EmailUsername = $FirstName + $LastName
     }
-    #Disable AD User
-    Set-ADUser $Username -Enabled $False
-    LogWrite "User $FullName is disabled"
-
-    #Get AD Groups and remove from all but Domain Users
-    $JoinedGroups = Get-ADPrincipalGroupMembership $Username | Where-Object {$_.Name -ne "Domain Users"}
-
-    LogWrite "Removing $FullName from:"
-    ForEach($Group in $JoinedGroups){ 
-    
-        $GroupName = $Group.Name
-        Remove-ADGroupmember -Identity $Group -Members $Username -Confirm:$False -ErrorAction SilentlyContinue
-        LogWrite "$Groupname"
-
+    ElseIf($EmailConvention -eq "FirstNameLastInitial"){
+        $EmailUsername = $FirstName + $LastInitial
     }
-    #Clear mail field in AD
-    Set-ADUser -Identity $Username -Clear Mail
-	#Reset user principal name domain to AD domain
-	Set-ADUser -Identity $Username -UserPrincipalName $Username@$env:USERDNSDOMAIN
-    #Set to hide from address lists
-    Set-ADUser -Identity $Username -Replace @{msExchHideFromAddressLists=$TRUE}
-    #Attribute must be set to sync in AD connect and a transform rule must be in place
-    Set-ADUser -Identity $Username -Replace @{"msDS-CloudExtensionAttribute1"="HideFromGAL"}
-    LogWrite "Removed $Fullname from global address list"
-
-    #Move user to "Disabled Users" OU
-    Get-ADUser $Username | Move-ADObject -TargetPath $DisabledUserPath
-    LogWrite "User $FullName has been moved to Disabled Users"
-
-    #Set mailbox to shared, set forwarding
-    If ($Mailbox){
-		If(-Not($Mailbox.isShared)){
-			Set-mailbox -Identity $Mailbox.alias -Type Shared
-			Write-Host "Setting mailbox for $FullName to Shared"
-		}
-		If($ForwardingAddress){
-			Set-mailbox -Identity $Mailbox.alias -ForwardingAddress $ForwardingAddress
-		}
+    ElseIf($EmailConvention -eq "Firstname.LastName"){
+        $Emailusername = $FirstName + "." + $LastName
+    }
+    ElseIf($EmailConvention -eq "FirstName"){
+        $EmailUsername = $FirstName
     }
     Else{
-        Write-Host "Could not set to shared. Please log into Office 365 to finish offboarding tasks"
+        $EmailUsername = $FirstInitial + $LastName
     }
+    If(-Not ($NewUser.EmailDomain)){
+        $EmailDomain = $NewUser.EmailDomain
+    }
+    $EmailAddr = "$EmailUsername@$EmailDomain"
+
     
-    If (($Mailbox.IsShared) -And ($MSolUser)){
-        Set-MsolUserLicense -UserPrincipalName $MsolUser.UserPrincipalName -RemoveLicenses $MSolUser.Licenses.AccountSkuId
-		Write-Host "Removed Office 365 license from $Fullname"
-		#Change user to .onmicrosoft.com format
-        Set-MsolUserPrincipalName -UserPrincipalName $MsolUser.UserPrincipalName -NewUserPrincipalName "$FLName@$MSDomain"
+    #Check if user exists
+    $UserExists = Get-ADUser -Filter {SamAccountName -eq $UserName} -ErrorAction SilentlyContinue
+    If (-Not ($UserExists)){
+        New-ADUser -Name $FullName -GivenName $FirstName -Surname $LastName -DisplayName $FullName -SamAccountName $UserName -AccountPassword $Password  -UserPrincipalName $Principal -Path $UserPath -Description $Description -Enabled:$True -Department $Department -Title $Description
+        Set-ADUser $Username -UserPrincipalName "$Username@$EmailDomain"
+        Write-Host "User $Username has been created"
+        Get-ADUser $Username
     }
     Else{
-        Write-Host "Mailbox is not shared. Did not remove license at this time."
+        Write-Host "User $Fullname already exists as $Username. Try a different username"
+        continue
     }
 
-    Write-Host "User offboarding for $FullName is complete"
+    $PrimarySMTP = "SMTP:"
+    $SecondarySMTP = "smtp:"
+
+    #Variable not used, may be removed in the future
+    #$UserFullName = $FirstName + $LastName
+
+    $UserMSEmail = "$Username@$ClientMSDomain"
+
+    $ProxyPrimary = $PrimarySMTP + $EmailAddr
+    $ProxySecondary = $SecondarySMTP + $UserMSEmail
+
+
+    Get-ADUser -Identity $Username | Set-ADUser -Add @{ProxyAddresses="$ProxyPrimary"}
+    Get-ADUser -Identity $Username | Set-ADUser -Add @{ProxyAddresses="$ProxySecondary"}
+    Get-AdUser -Identity $Username | Set-ADUser -Replace @{mail="$EmailAddr"}
+    #Get-AdUser -Identity $Username | Set-ADUser -Replace @{title="$Description"}
+
+        #Copy groups from another user
+    If($UserToCopy -ne "None"){
+        $SourceUser = Get-ADUser $UserToCopy -ErrorAction SilentlyContinue
+        }
+
+    If(-Not ($SourceUser)){
+        $UserGroups = Get-ADPrincipalGroupMembership $SourceUser | Where-Object {$_.Name -ne "Domain Users"}
+        ForEach($Group in $UserGroups){
+            $GroupName = $Group.name
+            Add-ADGroupMember -Identity $Group -Members $Username -ErrorAction SilentlyContinue
+            Write-Host "Copying user $Username to $Groupname"
+            }
+
+        }
+
+    Else{
+        Write-Host "No groups to copy"
+    }
+    
 }
 
 SyncADtoO365
